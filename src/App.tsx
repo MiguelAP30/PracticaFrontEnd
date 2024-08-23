963a9eea7ece1ea4a297f5a873a6e7af2bb65a92
MiguelAP30 <miguelonperez300@gmail.com>
2024-08-23 17:06:19 -0500

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import {Principal} from './components/Principal'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Principal />
+      <Footer />
+    </>
+  )
+}
+
+export default App
